df101e64b2179867c3c27fe2f0b037da5a682317
Tomasz Mazur <tomimas231@gmail.com>
2023-03-31 10:57:14 +0200

    Change class views to functions | cart, checkout, update-cart views

--- shop/urls.py
+++ shop/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.ProductListView.as_view(), name='home'),
-    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
+    path("", views.store, name='home'),
+    path('products/<int:id>/', views.product_detail, name='product-detail'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name="checkout"),
+    path('update_item', views.update_item, name='update_item'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import DetailView, ListView
+from django.http import JsonResponse
+import json
 from .models import *
 
 
-class ProductListView(ListView):
-    model = Product
-    template_name = 'shop/home.html'
-    context_object_name = 'products'
+def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        cart_items_counter = order.items_counter
+    else:
+        # logic for guest user
+        order = {'items_counter': 0, 'total_price': 0}
+        cart_items_counter = order['items_counter']
+
+    products = Product.objects.all()
+    context = {'products': products, 'cart_items_counter': cart_items_counter}
+    return render(request, 'shop/home.html', context)
 
 def cart(request):
     if request.user.is_authenticated:
@@ -15,12 +26,14 @@ def cart(request):
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         # get all order items
         items = order.orderitem_set.all()
+        cart_items_counter = order.items_counter
     else:
         #TODO displaying items and order for unlogged users
         items = []
         order = {'items_counter': 0, 'total_price': 0}
+        cart_items_counter = order['items_counter']
     
-    context = {"items": items, "order": order}
+    context = {"items": items, "order": order, 'cart_items_counter': cart_items_counter}
     return render(request, 'shop/cart.html', context)
 
 def checkout(request):
@@ -28,13 +41,52 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cart_items_counter = order.items_counter
     else:
         items = []
         order = {'items_counter': 0, 'total_price': 0}
+        cart_items_counter = order['items_counter']
     
-    context = {"items": items, "order": order}
+    context = {"items": items, "order": order, 'cart_items_counter': cart_items_counter}
     return render(request, 'shop/checkout.html', context)
 
-class ProductDetailView(DetailView):
-    model = Product
-    context_object_name = 'product'
+def update_item(request):
+    # this view work only for logged user!
+
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:', action)
+    print('Product:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    # get order attached to the customer
+    order, created = Order.objects.get_or_create(customer=customer, complete = False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity += 1
+    elif action == 'remove':
+        orderItem.quantity -= 1
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
+
+def product_detail(request, id):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        cart_items_counter = order.items_counter
+    else:
+        # logic for guest user
+        order = {'items_counter': 0, 'total_price': 0}
+        cart_items_counter = order['items_counter']
+
+    product = get_object_or_404(Product, id=id)
+    context = {'product': product, 'cart_items_counter': cart_items_counter}
+    return render(request, 'shop/product_detail.html', context=context)
